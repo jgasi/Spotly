27482c41612cd95e34e1bac855e68d8b084b5493
jcivcija <jcivcija21@student.foi.hr>
2024-11-25 21:46:47 +0100

    Napravljen Activity za rezervaciju parkinga

--- frontend/app/src/main/java/org/foi/hr/air/spotly/ReserveParkingSpaceActivity.kt
+++ frontend/app/src/main/java/org/foi/hr/air/spotly/ReserveParkingSpaceActivity.kt
@@ -12,11 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.foi.hr.air.spotly.ui.theme.SpotlyTheme
 
-class ReserveParkingSpace : ComponentActivity() {
+class ReserveParkingSpaceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,32 +39,9 @@ fun ReserveParkingSpaceScreen() {
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Zahtjev za izmjenu podataka profila",
+            text = "Garaža parking",
             style = androidx.compose.material3.MaterialTheme.typography.titleLarge,
             modifier = Modifier.padding(bottom = 24.dp)
         )
-
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Novo ime") },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
-
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Novi email") },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
-
-        Button(
-            onClick = {
-                // Tu će ići logika za slanje zahtjeva u backend
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Pošaljite zahtjev")
-        }
     }
 }
