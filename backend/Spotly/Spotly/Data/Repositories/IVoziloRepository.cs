90920d52d0d2cc0843895c3a289ce4cbf21cd463
Matej Franjić <mfranjic21@student.foi.hr>
2024-12-12 17:28:25 +0100

    Dohvat korisnikovog vozila te prikaz na stranici ProfilePage
    Signed-off-by: Matej Franjić <mfranjic21@student.foi.hr>

--- backend/Spotly/Spotly/Data/Repositories/IVoziloRepository.cs
+++ backend/Spotly/Spotly/Data/Repositories/IVoziloRepository.cs
@@ -5,6 +5,7 @@ namespace Spotly.Data.Repositories
 	public interface IVoziloRepository : IRepository<Vozilo>
     {
         Task<Vozilo> GetByLicensePlateAsync(string licensePlate);
+        Task<Vozilo> GetVoziloByKorisnikIdAsync(int id);
     }
 }
 
