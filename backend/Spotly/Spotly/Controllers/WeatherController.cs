67b126ff38f2cc826a0dcbe51d9b188ffdc1a515
jcivcija <jcivcija21@student.foi.hr>
2024-12-31 19:45:35 +0100

    Weather API fetches weather

---
+++ backend/Spotly/Spotly/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spotly.DTOs;
+using Spotly.Services;
+
+namespace Spotly.Controllers
+{
+    [ApiController]
+    [Route("api/weather")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherController(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<WeatherResponse>> GetWeather()
+        {
+            var weatherData = await _weatherService.GetWeatherAsync();
+
+            if (weatherData == null)
+            {
+                return NotFound("Weather data could not be retrieved.");
+            }
+
+            return Ok(weatherData);
+        }
+    }
+}
---
+++ backend/Spotly/Spotly/Services/WeatherBackgroundService.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Spotly.Services
+{
+    public class WeatherBackgroundService : BackgroundService
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherBackgroundService(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var weather = await _weatherService.GetWeatherAsync();
+                    Console.WriteLine($"Temperature: {weather.Weather[0].Main}, Condition: {weather.Weather[0].Description}");
+                    if (weather.Weather[0].Main == "Snow" || weather.Weather[0].Main == "Thunderstorm")
+                    {
+                        //TODO: Send notification
+                    }
+                } catch (Exception ex)
+                {
+                    Console.WriteLine($"Error fetching weather: {ex.Message}");
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+    }
+}
