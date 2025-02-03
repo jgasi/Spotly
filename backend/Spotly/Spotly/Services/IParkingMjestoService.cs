ceaf3e169d106a621ed2e9fc9cde7896252b2d6b
Toni <tivanovic21@student.foi.hr>
2025-02-03 09:35:12 +0100

    .

--- backend/Spotly/Spotly/Services/IParkingMjestoService.cs
+++ backend/Spotly/Spotly/Services/IParkingMjestoService.cs
@@ -5,5 +5,16 @@ namespace Spotly.Services
     public interface IParkingMjestoService
     {
         Task<IEnumerable<ParkingMjesto>> GetAllParkingMjestaAsync();
+        Task<ParkingMjesto> GetParkingMjestoByIdAsync(int id);
+
+        Task<int> GetTotalParkingMjestaAsync();
+        Task<int> GetAvailableParkingMjestaAsync();
+        Task<int> GetReservedParkingMjestaAsync();
+        Task<int> GetBlockedParkingMjestaAsync();
+        Task<bool> BlokirajParkingMjestoAsync(int id);
+        Task<bool> OdblokirajParkingMjestoAsync(int id);
+        Task<IEnumerable<ParkingMjesto>> GetAllParkingMjestaIncludeAsync();
+        Task<ParkingMjesto> GetParkingMjestoById(int id);
+        Task UpdateParkingMjesto(ParkingMjesto parkingMjesto);
     }
 }
